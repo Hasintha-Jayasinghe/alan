9f7ef9bf1917066ec4e49bcbb752c8254891443f
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2020-12-24 15:03:19 +0530

    Deploy 3

--- src/components/visualizer/Visualizer.tsx
+++ src/components/visualizer/Visualizer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import p5 from "p5";
 import "p5/lib/addons/p5.sound";
+import "./Visualizer.css";
 
 interface Props {
   on: boolean;
@@ -8,23 +9,27 @@ interface Props {
 
 const Visualizer: React.FC<Props> = ({ on }) => {
   const app = useRef<any>(null);
-  let mic: p5.AudioIn;
+  const mic = useRef<any>(null);
 
   useEffect(() => {
     const newp5 = new p5((p: p5) => {
+      p.touchStarted = () => {
+        new AudioContext().resume().then(() => console.log("Done..."));
+      };
+
       p.setup = () => {
         p.createCanvas(500, 500);
-        mic = new p5.AudioIn();
+        mic.current = new p5.AudioIn();
         if (on) {
-          mic.start();
+          mic.current.start();
         } else {
-          mic.stop();
+          mic.current.stop();
         }
       };
 
       p.draw = () => {
         p.background(255);
-        const vol = mic.getLevel();
+        const vol = mic.current.getLevel();
         p.noStroke();
         p.ellipse(250, 250, vol * 200, vol * 200);
         p.fill(96, 96, 252);
@@ -36,7 +41,7 @@ const Visualizer: React.FC<Props> = ({ on }) => {
     };
   }, [on]);
 
-  return <div ref={app}></div>;
+  return <>{on ? <div ref={app} /> : null}</>;
 };
 
 export default Visualizer;
